6a0b025b6ce9de303feb11a87f8d81748d06e119
ShibamD <>
2025-07-11 10:14:35 +0000

    Project init

---
+++ README.md
@@ -0,0 +1,32 @@
+# onv-java-test
+
+Generated Java service from IDP template
+
+**Owner:** Sdhar
+
+## Getting Started
+
+### Prerequisites
+- Java 17+
+- Maven 3.6+
+
+### Build and Run
+```bash
+# Compile
+mvn compile
+
+# Run
+mvn exec:java -Dexec.mainClass="abc.App"
+
+# Package
+mvn package
+```
+
+### Development
+```bash
+# Clean build
+mvn clean compile
+
+# Run tests
+mvn test
+```
---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+package abc;
+
+/**
+ * onv-java-test - Generated Java service from IDP template
+ * 
+ * @author Sdhar
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to onv-java-test!");
+        System.out.println("Description: Generated Java service from IDP template");
+        System.out.println("Maintained by: Sdhar");
+    }
+}
